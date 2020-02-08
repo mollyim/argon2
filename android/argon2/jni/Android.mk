f1869c228450d88eb7893bcbffc0be0dce1758f2
Oscar Mira <valldrac@molly.im>
2020-02-08 11:46:26 +0100

    Remove precompiled libs and use native build

--- android/argon2/jni/Android.mk
+++ android/argon2/jni/Android.mk
@@ -1,13 +1,16 @@
-LOCAL_DIR := $(call my-dir)
-ARGON2_DIR := $(LOCAL_DIR)/../../../phc-winner-argon2
+LOCAL_PATH := $(call my-dir)
+ARGON2_DIR := $(LOCAL_PATH)/../../../phc-winner-argon2
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := argon2
 LOCAL_C_INCLUDES := $(ARGON2_DIR)/include/
 LOCAL_CFLAGS     += -Wall
+ifeq ($(APP_OPTIM),release)
+LOCAL_LDLIBS     += -Wl,--build-id=none
+endif
 
-LOCAL_SRC_FILES := $(LOCAL_DIR)/org_signal_argon2_Argon2Native.c \
+LOCAL_SRC_FILES := org_signal_argon2_Argon2Native.c \
                    $(ARGON2_DIR)/src/blake2/blake2b.c \
                    $(ARGON2_DIR)/src/argon2.c \
                    $(ARGON2_DIR)/src/core.c \
